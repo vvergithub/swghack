3380cfc5be20dfbaca3c4f7b1111a50c49ddb2c3
unknown <VVER@LT-789408.statoil.net>
2015-10-19 11:57:19 +0200

    Another port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = (process.env.PORT || '3000');
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('PORT: ' + port);
 });
 
 var server = app.listen(port, function () {
