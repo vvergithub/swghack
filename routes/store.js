b42e07d07a3f386afc3129afd6b504c5e1fd914c
unknown <VVER@LT-789408.statoil.net>
2015-10-19 14:16:06 +0200

    added message url

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('store', { title: 'ExpressStore' });
+    res.render('store',{title: 'store',  messageURL : process.env.MESSAGE_URL});
 });
 
 router.route('/message').post(function(req, res, next) {
